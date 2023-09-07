4d3584419c133fe2938dee65a4ca85de79dd2c37
Nicholas Stevens <74801713+NicholasStevens@users.noreply.github.com>
2023-09-07 12:34:16 +0200

    add sections for mass nd volume, add css, add reverse calculations

--- index.js
+++ index.js
@@ -6,13 +6,32 @@
 let calcBtn = document.getElementById("calc-btn");
 let inputEl = document.getElementById("input");
 let lengthEl = document.getElementById("length-el");
+let volumeEl = document.getElementById("volume-el");
+let massEl = document.getElementById("mass-el");
 
 const meterToFeet = 3.281;
-const literToGallon = 0.264;
+const feetToMeter = 0.3048;
+const litreToGallon = 0.264;
+const gallonToLitre = 3.78541;
 const kiloToPound = 2.204;
+const poundToKilo = 0.45359237;
 
 calcBtn.addEventListener("click", function () {
   let baseValue = inputEl.value;
 
-  lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet`;
+  lengthEl.textContent = `${baseValue} meters = ${
+    baseValue * meterToFeet
+  } feet | ${baseValue} feet = ${(baseValue * feetToMeter).toFixed(2)} meters`;
+  volumeEl.textContent = `${baseValue} litres = ${
+    baseValue * litreToGallon
+  } gallons | ${baseValue} gallons is ${(baseValue * gallonToLitre).toFixed(
+    2
+  )} litres`;
+  massEl.textContent = `${baseValue} kilograms = ${
+    baseValue * kiloToPound
+  } pounds | ${baseValue} pounds = ${(baseValue * poundToKilo).toFixed(
+    2
+  )} kilos`;
+
+  inputEl.value = "";
 });
