cfd9d1907032e7bd136f48d1ecade6b90a10f92b
Nicholas Stevens <74801713+NicholasStevens@users.noreply.github.com>
2023-09-07 11:23:19 +0200

    setup and first section js complete

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+let calcBtn = document.getElementById("calc-btn");
+let inputEl = document.getElementById("input");
+let lengthEl = document.getElementById("length-el");
+
+const meterToFeet = 3.281;
+const literToGallon = 0.264;
+const kiloToPound = 2.204;
+
+calcBtn.addEventListener("click", function () {
+  let baseValue = inputEl.value;
+
+  lengthEl.textContent = `${baseValue} meter = ${baseValue * meterToFeet} feet`;
+});
